cac6a19c03749b90cc3951e49f6502edf14b4137
eigeneddie <66312407+eigeneddie@users.noreply.github.com>
2021-11-11 17:58:25 +0700

    significantly modified activeMode script to be MORE READABLE AND STRUCTURED

--- arduino_scripts/active_modes/active_modes.ino
+++ arduino_scripts/active_modes/active_modes.ino
@@ -24,14 +24,10 @@
 #define HX711_dout 4 //mcu > HX711 dout pi
 #define HX711_sck 5  //mcu > HX711 sck pin
 
-
 // 2. initiate objects 
 AccelStepper motor_actuator(1, stepperPulse, stepperDirection);
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 VL53L0X distance_sensor;
-
-
-//HX711 constructor:
 
 // 3. universal string command
 String stringCommand;
@@ -71,6 +67,7 @@ float measuredAngle = 0; // value read from pot
 long maxMotorSpeed;
 float max_angle; // maximum angle limit
 float min_angle; // minimum angle limit
+volatile long assignedSpeed; // speed to chase knee angle
 
 // 8. activation parameters for ISR
 int count_pidGo = 0; // to down sample the proportional control
@@ -78,33 +75,37 @@ bool pidGo = false; // Go-NoGo for controller (angle tracking)
 bool activeGo = false; // Go-NoGo for active admittance
 bool led_state = LOW; // LED indicator
 
-// 9. moving motor
-volatile long assignedSpeed;
-
-// 10. Max speeds for functions in ISR
+// 9. Max speeds for functions in ISR
 long max_active_mode = 1000; // [step/s]
 long max_proportional_mode = 1500; // [step/s]
 
-// 11. Knee angle monitoring 
+// 10. Knee angle monitoring 
 // (undersampled and printed to serial monitor)
 unsigned long startTime_dur;
 unsigned long startTime; // start time
 unsigned long currentTime; // current time
 const unsigned long period = 2000; //undersampling data period
 
-// 12. HX711 utilities
+// 11. HX711 utilities
 unsigned long t = 0;
 static boolean newDataReady = 0;
 float measuredForce = 0;
+
+// 12. VLX Distance sensor utils
+float sliderDistance; // Origin is at theta = max_angle
+float zeroDistance; // correcting value to sliderDistance
+float sensorReading; // actual mm reading of sensor
+// note: sliderDistance = zeroDistance-sensorReading
 
 void setup() {
   Serial.begin(115200); //Serial.setTimeout(500);
   Wire.begin();
 
+  Serial.println("Initiating Rehab-Bot"); Serial.println(" ");
+
   //=== I. Basic system functionality ===
   //  - Stepper motor setup
   //  - Limit switch 
-  //  - 
 
   // -> Stepper motor pin setup and init
   pinMode(stepperPulse, OUTPUT);
@@ -134,8 +135,9 @@ void setup() {
   while(LoadCell.getTareTimeoutFlag()){
     Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
     Serial.println("Retrying...")
+    delay(200);
   }
-  
+  Serial.println(" ");
   LoadCell.setCalFactor(calibrationValue);
   Serial.println("INIT_2: Load cell setup complete");
   
@@ -150,24 +152,22 @@ void setup() {
   Serial.println("distance sensor detected");
   delay(500);
 
-  // Set for high accuracy, timing budget 200 ms
-  distance_sensor.setMeasurementTimingBudget(200000);
+  //Note: to get distance, use: 
+  // distance_sensor.readRangeSingleMillimeters();
+  // distance_sensor.startContinuous();
+  // distance_sensor.stopContinuous();
+  // distance_sensor.readRangeContinuousMillimeters(); -->
+  //sensor.setMeasurementTimingBudget(100000); --> change accuracy
 
-  //Note: to get distance, use:
-  //distance_sensor.readRangeSingleMillimeters();
-   
   //=== IV. Internal Interrupt and Closed-loop Control Init ===
-  // -> init PID and internal interrupt setting
-
-  
   // ->Enable PID and internal interrupt
   init_pid(Kp, Ki, Kd, N, Ts); 
   TimerInit();
 }
 
 void loop() {
-
   // 1. Null: Loop level, set motor speed to zero
+  activeGo = false;
   motor_actuator.setSpeed(0); 
   motor_actuator.runSpeed(); // making sure the motor stops when command stops
   
@@ -176,7 +176,7 @@ void loop() {
   
   // 3. checking knee angle
   read_angle();
-  print_data();
+  print_data("void");
 }
 
 void check_serial(){
@@ -248,7 +248,7 @@ void check_serial(){
      * "3;0;an;bn;bn1;max_angle;min_angle\n"
      */ 
     if(stringCommand.charAt(0) == '3' && stringCommand.charAt(1) == '0'){
-      Serial.println("Initiating mode '3'"); Serial.println(" ");
+      Serial.println("Initiating mode '3' Isotonic"); Serial.println(" ");
       pidGo = false;
       activeGo = false;
       
@@ -256,7 +256,7 @@ void check_serial(){
       // a. parse command
       Serial.println("a. parsing command"); Serial.println(" ");
       delay(500);
-      active_isotonic(stringCommand); // parsing single command
+      parse_active_isotonic(stringCommand); // parsing single command
 
       // b. zero proportional control
       Serial.println("b. zeroing control param"); Serial.println(" ");
@@ -277,41 +277,49 @@ void check_serial(){
       Serial.println("Tare complete."); Serial.println(" ");
       delay(500);
 
+      // e. slider position
+      distance_sensor.setMeasurementTimingBudget(200000); //set measurements for high accuracy
+      zeroDistance = distance_sensor.readRangeSingleMillimeters(); //read position once
+      sliderDistance = zeroDistance - sensorReading; //current slider position from origin
+      distance_sensor.setMeasurementTimingBudget(50000); //set measurements for high speed (takes up to 50 ms on loop)
+      distance_sensor.startContinuous(); //read distance continuously
+
+      // f. enter program
       Serial.println("Entering mode '3': isotonic");
       delay(1000);
       activeGo = true;
-      startTime = millis();      
+      startTime = millis();
 
-      while(activeGo){
+      while(activeGo){ // EXECUTE ACTIVE TRAINING MODE
 
         // a. Read force sensor
-        static boolean newDataReady = 0;
-        const int serialPrintInterval = 2000; //increase value to slow down serial print activity
-        LoadCell.update(); // check for new data/start next conversion
-        measuredForce = LoadCell.getData(); // get smoothed value from the dataset
-        
-        // b. run motor
-        motor_actuator.moveTo(step_target_n);
-        motor_actuator.run();
+        read_force();
 
-        // c. Measured angle
+        // b. move motor
+        move_motor();
+
+        // c. Current measured angle
         read_angle();
 
-        // d. Stoping criteria (force stop "-s")
+        // d. Current measure slider position
+        read_slider_position();
+        
+        // e. Stoping criteria (force stop "-s")
         stopping_criteria_hap_ren();
         
         // e. undersampling print
-        print_data("mode30");
+        print_data("mode3Isotonic");
 
         // f. check if measured angle <= min_angle
-        back_to_flexion();
-
+        back_to_flexion_2();
       }
     }
   } 
 }
 
-// Active mode '2' 
+// FUNCTIONS I: Parsing commands
+//------------------------------------------
+// I.a. Active mode '2': Assistive control 
 void assistive_control (String command_data){
   String acoef1 = getValue(command_data, ';', 2);
   String bcoef0 = getValue(command_data, ';', 3);
@@ -326,8 +334,8 @@ void assistive_control (String command_data){
   min_angle = minAng.toFloat();
 }
 
-// Active mode '3' & '0'
-void active_isotonic (String command_data){
+// I.b. Active mode '3' & '0': Isotonic training
+void parse_active_isotonic (String command_data){
   String acoef1 = getValue(command_data, ';', 2);
   String bcoef0 = getValue(command_data, ';', 3);
   String bcoef1 = getValue(command_data, ';', 4);
@@ -341,8 +349,8 @@ void active_isotonic (String command_data){
   min_angle = minAng.toFloat();
 }
 
-// Active mode '3' & '1'
-void active_isometric (String command_data){
+// I.c. Active mode '3' & '1': Isometric training
+void parse_active_isometric (String command_data){
   String acoef1 = getValue(command_data, ';', 2);
   String bcoef0 = getValue(command_data, ';', 3);
   String bcoef1 = getValue(command_data, ';', 4);
@@ -356,15 +364,29 @@ void active_isometric (String command_data){
   min_angle = minAng.toFloat();
 }
 
-float haptic_rendering(float measured_force){
-  y_n = a_coef_1*y_n_1 + b_coef_0*f_n + b_coef_1*f_n_1;
-  long position_target = (long) y_n*50.0; // 1/8 rev/mm * 400 steps/rev 
-  f_n_1 = f_n;
-  y_n_1 = y_n;
-  return position_target;
+// I.d parsing string commands
+String getValue(String command_data, char separator, int index){
+ /* This code is thanks to the people of stackOverflow <3!!
+  * convinient for extracting command usage.
+  */
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = command_data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(command_data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+  }
+
+  return found>index ? command_data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-// return knee angle to flexion (max_angle) position
+// FUNCTIONS II: Return to flexion position
+//------------------------------------------
+// II.a. at init, return knee angle to flexion (max_angle) position
 void back_to_flexion(){
   if (measuredAngle != max_angle){
     maxMotorSpeed = max_proportional_mode;
@@ -381,54 +403,84 @@ void back_to_flexion(){
       motor_actuator.setSpeed(assignedSpeed);
       motor_actuator.runSpeed();
       
+      // iv. print data
+      print_data("back to flexion")
       stopping_criteria_angleCont();
     }
   }
 }
 
-// get current angle sensor reading
+// II.b. during training and at min_angle, 
+//       return knee angle to flexion (max_angle) position
+void back_to_flexion_2(){
+  if (measuredAngle == min_angle){
+    maxMotorSpeed = max_proportional_mode;
+    pidGo = true;
+    activeGo = false;
+    while(pidGo){
+      // i. measure angle
+      read_angle();
+
+      // ii. set target angle
+      targetAngle = max_angle;
+
+      // iii. run motor
+      motor_actuator.setSpeed(assignedSpeed);
+      motor_actuator.runSpeed();
+      
+      // iv. print data
+      print_data("back to flexion")
+      stopping_criteria_hap_ren();
+    }
+  }
+}
+
+// FUNCTIONS III: Read sensors, store, and run motor
+//------------------------------------------
+// III.a move the motor to target position
+void move_motor(){
+  motor_actuator.moveTo(step_target_n);
+  motor_actuator.run();
+}
+
+// III.b read force data
+void read_force(){
+  static boolean newDataReady = 0;
+  const int serialPrintInterval = 2000; //increase value to slow down serial print activity
+  LoadCell.update(); // check for new data/start next conversion
+  measuredForce = LoadCell.getData(); // get smoothed value from the dataset    
+}
+
+// III.d read current knee angle
 void read_angle(){
   sensorValue = analogRead(angleSensorPin);
   measuredAngle = float(map(sensorValue, 0, 1023, 0, 333)-offsetAngle);
 }
+// III.e read current slider position
+void read_slider_position(){
+  sliderDistance = zeroDistance - distance_sensor.readRangeContinuousMillimeters();
 
-// print current data to screen
+}
+
+// III.f print current data to screen/send to high-level controller
+// WARNING, printing at higher frequencies can degrade control performance
 void print_data(String mode){
   currentTime = millis(); // grab time
   if (currentTime-startTime >= period){
     Serial.print(mode); Serial.print(" ");
     Serial.print(measuredAngle); Serial.print(" ");
-    Serial.print(u0); Serial.print(" ");
+    Serial.print(sliderDistance); Serial.print(" ");
     Serial.print(measuredForce); Serial.println(" ");
     startTime = currentTime;
   }  
 }
-// stopping criteria for angle tracking (proportional control)
-void stopping_criteria_angleCont(){
-  if (Serial.available() > 0){
-    stringCommand = Serial.readStringUntil('\n');
-    if (stringCommand == "-s"){
-      pidGo = false;
-      activeGo = true;
-    }
-  }
-}
 
-// stopping criteria for haptic rendering
-void stopping_criteria_hap_ren(){
-  if (Serial.available() > 0){
-    stringCommand = Serial.readStringUntil('\n');
-    if (stringCommand == "-s"){
-      activeGo = false;
-    }
-  }
-}
-
-// ISR for executing angle proporsional control & haptic rendering
+// FUNCTIONS IV: Main control functions (inside Internal Interrupt, TIMER1)
+//------------------------------------------
+// IV.a ISR for executing haptic rendering & proportional control
 ISR(TIMER1_COMPA_vect){
-  /* Interrupt service routine for
-   *  PID execution
-   */
+  
+  //====== proportional control =======
   if (pidGo == true){
     count_pidGo++;
     if(count_pidGo>49){
@@ -437,48 +489,38 @@ ISR(TIMER1_COMPA_vect){
     }
     digitalWrite(LED_BUILTIN, LOW);
   } 
-  else if (pidGo == false){
+  else {
     assignedSpeed = 0;
   }
-  
+  //==================================
+
+  //====== haptic rendering =======
   if (activeGo == true){
     step_target_n = haptic_rendering(measuredForce);
     digitalWrite(LED_BUILTIN, HIGH);
   }
+  //==================================
 }
 
-// Handles internal interrupt execution
-void TimerInit(){
-  /* Initialize timer1 
-   * Sampling Frequency = 100 Hz 
-   * Timer for PID loop in passive training (angle control)  
-   * 
-   * Study reference:
-   * https://www.robotshop.com/community/forum/t/arduino-101-timers-and-interrupts/13072
-   * 
-   */ 
-  noInterrupts(); //disable all interrupts
-  TCCR1A = 0; // Timer or Counter Control Register
-  TCCR1B = 0; // The prescaler can be configure in TCCRx
-  TCNT1  = 0; // Timer or Counter Register. The actual timer value is stored here.
-
-  OCR1A = 1249; // Output Compare Match Register (16Mhz/256/<sampling_freq>Hz)
-  //62499 (1 Hz);//31249 (2Hz); //15625 (4Hz) //6249 (10Hz); //1249 (50Hz);
-  //624 (100Hz); 
-
-  TCCR1B |= (1 << WGM12);  // CTC (Clear Time on Compare Match)
-  TCCR1B |= (1 << CS12);   // 256 prescaler
-  TIMSK1 |= (1 << OCIE1A); // Interrupt Mask Register (Enable Output Compare Interrupt)
-  interrupts();            // Enable all interrupts
-}
-
+// IV.b zero every control parameters (haptic rendering and proportional control)
 void zero_everything(){
   e2 = 0, e1 = 0, e0 =0;
   u2 = 0, u1 = 0, u0 = 0; 
   f_n = 0, f_n_1 = 0;
   y_n = 0, y_n_1 = 0;
+  step_target_n = 0;
 }
 
+// IV.c Resistance Algorithm (admittance rendering) 
+float haptic_rendering(float measured_force){
+  y_n = a_coef_1*y_n_1 + b_coef_0*f_n + b_coef_1*f_n_1;
+  long position_target = (long) y_n*50.0; // 1/8 rev/mm * 400 steps/rev 
+  f_n_1 = f_n;
+  y_n_1 = y_n;
+  return position_target;
+}
+
+// IV.d initialize proportional control
 void init_pid(float Kp, float Ki, float Kd, float N, float Ts){
   /* Initializing PID equation based on user defined parameters
    * 
@@ -506,6 +548,7 @@ void init_pid(float Kp, float Ki, float Kd, float N, float Ts){
   ke2 = b2/a0;
 }
 
+// IV.e proportional control execution
 float pid_execute(float target_angle, float plant_output, long speed_sat){
   /* Executing PID control algorithm
    * 
@@ -537,21 +580,51 @@ float pid_execute(float target_angle, float plant_output, long speed_sat){
   return u0;
 }
 
-String getValue(String command_data, char separator, int index){
- /* This code is thanks to the people of stackOverflow <3!!
-  * convinient for extracting command usage.
-  */
-  int found = 0;
-  int strIndex[] = {0, -1};
-  int maxIndex = command_data.length()-1;
+// IV.f Customizes internal interrupt counters
+void TimerInit(){
+  /* Initialize timer1 
+   * Sampling Frequency = 100 Hz 
+   * Timer for PID loop in passive training (angle control)  
+   * 
+   * Study reference:
+   * https://www.robotshop.com/community/forum/t/arduino-101-timers-and-interrupts/13072
+   * 
+   */ 
+  noInterrupts(); //disable all interrupts
+  TCCR1A = 0; // Timer or Counter Control Register
+  TCCR1B = 0; // The prescaler can be configure in TCCRx
+  TCNT1  = 0; // Timer or Counter Register. The actual timer value is stored here.
 
-  for(int i=0; i<=maxIndex && found<=index; i++){
-    if(command_data.charAt(i)==separator || i==maxIndex){
-        found++;
-        strIndex[0] = strIndex[1]+1;
-        strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
+  OCR1A = 1249; // Output Compare Match Register (16Mhz/256/<sampling_freq>Hz)
+  //62499 (1 Hz);//31249 (2Hz); //15625 (4Hz) //6249 (10Hz); //1249 (50Hz);
+  //624 (100Hz); 
+
+  TCCR1B |= (1 << WGM12);  // CTC (Clear Time on Compare Match)
+  TCCR1B |= (1 << CS12);   // 256 prescaler
+  TIMSK1 |= (1 << OCIE1A); // Interrupt Mask Register (Enable Output Compare Interrupt)
+  interrupts();            // Enable all interrupts
+}
+
+// FUNCTIONS V: loop stopping criteria
+//------------------------------------------
+// V.a stopping criteria for angle tracking (proportional control)
+void stopping_criteria_angleCont(){
+  if (Serial.available() > 0){
+    stringCommand = Serial.readStringUntil('\n');
+    if (stringCommand == "-s"){
+      pidGo = false;
+      activeGo = true;
+    }
   }
+}
 
-  return found>index ? command_data.substring(strIndex[0], strIndex[1]) : "";
+// V.b stopping criteria for haptic rendering
+void stopping_criteria_hap_ren(){
+  if (Serial.available() > 0){
+    stringCommand = Serial.readStringUntil('\n');
+    if (stringCommand == "-s"){
+      pidGo = false;
+      activeGo = false;
+    }
+  }
 }
